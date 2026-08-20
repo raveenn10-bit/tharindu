9d221a01ae1e6968ec36ee2d0278072ca884956b
raveenn10-bit <raveenn10-bit@users.noreply.github.com>
2026-08-20 22:01:05 +0530

    chore: remove under development notice banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { ContentProvider, useContent } from './context/ContentContext'
-import UnderDevelopmentNotice from './components/UnderDevelopmentNotice'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -99,9 +98,6 @@ function MainSite() {
 
       {/* WhatsApp Floating Action Button */}
       <WhatsAppFloatingButton />
-
-      {/* Under Development Top Announcement Notice */}
-      <UnderDevelopmentNotice />
 
       {/* Global Transparent Navbar */}
       <Navbar />
